5ef676da80816d3536c091310bbf1b56a79f3fc8
wilmer3004 <131203913+wilmer3004@users.noreply.github.com>
2023-05-27 22:10:06 -0500

    Se termino el capitulo 1
    Se realizo la finalización del capitulo 1 del video junior

---
+++ JavaScript/Junior/Ejercicio1/Ejercicio/Capitulo1/app.js
@@ -0,0 +1,69 @@
+// alert('hola');
+// recipiente = "pedro";
+// alert(recipiente);
+// String = "Cadena de texto";
+// Number = 19;
+// Boolean=true;
+// let numero = 15;
+// const nombre = 'Wilmer';
+// alert(nombre);
+// let numero = 23;
+// numero = 29;
+// alert (numero);
+// let numero1=23, numero2=29;
+// alert(numero1 + " " + numero2);
+// let numero = null;
+// let nombre = prompt("Dime  tu nombre");
+// alert('Hola ' + nombre);
+// Operadores de asignación
+/*
+x=y
+x+=y
+x-=y
+x*=y
+x/=y
+x%=y
+x**=y asignación de exponentiation
+x<<=y asignación de desplazamiento
+x>>=y
+x>>>=y
+x&=y
+x^=y
+x|=y
+
+Ejemplos
+
+let numero = 10;
+ numero +=5;
+document.write(numero);
+*/
+// Operadores aritméticos
+
+// numero1 = 10;
+// numero2 = 5;
+// resultado = numero1;
+// alert(resultado);
+
+// Concatenación
+
+// let saludo = '¡Hola pedro!';
+// let pregunta = '¿Como estas?';
+// frase = saludo + pregunta;
+// document.write(frase);
+
+// Concatenación con backticks
+// frase1 = "Wilmer";
+// frase = `¿hola como estas ${frase1} ? `;
+// document.write(frase);
+
+// Operadores de comparación
+// let numero = 23;
+// let numero2 = 13;
+
+// document.write(numero<=numero2);
+
+// if (10>15) {
+//    alert('hola'); 
+// }
+
+
